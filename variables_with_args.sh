8a1b940ecfba92910d6a2f47b25d3dfe46590480
JagathReddy <jagathsaipj81@gmail.com>
2026-02-17 19:09:26 +0530

    passing values by arguments for variables

---
+++ variables_with_args.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1 :: Hi $PERSON2 wanna go for a trip "
+echo "$PERSON2 : I am always ready $PERSON1 "
+echo "$PERSON1 : Pack you bags $PERSON2, will be leaving by tomorrow"
+echo "$PERSON2 : thats was quick $PERSON1, but i will be on time"
