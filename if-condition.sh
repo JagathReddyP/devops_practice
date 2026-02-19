9622b6bc74363733a6a0db1b52ea94f2d34db679
JagathReddy <jagathsaipj81@gmail.com>
2026-02-20 00:50:07 +0530

    smaple if condition

--- if-condition.sh
+++ if-condition.sh
@@ -4,6 +4,8 @@ number=$1
 
 echo "given number is: $number"
 
+# -eq equal, -ne not equal, -gt	greater than, -lt less than, -ge greater or equal, -le	less or equal
+
 if [ $number -gt 20 ] 
 then
     echo " $number is greater than 20"
