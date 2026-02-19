e2bfbd0929d89c6799b815f18a383a99de351d70
JagathReddy <jagathsaipj81@gmail.com>
2026-02-19 11:22:00 +0530

    special varaibles & smaple if condition

--- if-condition.sh
+++ if-condition.sh
@@ -4,11 +4,12 @@ number=$1
 
 echo "given number is: $number"
 
-if($number>20)
+if($number>20){
 {
     echo "given $number is greater than 20"
 }
 else
 {
     echo "given $number is less than 20"
+}
 }
