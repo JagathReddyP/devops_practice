c485472603b71852a9022291e959d36702a9ea9e
JagathReddy <jagathsaipj81@gmail.com>
2026-02-21 01:17:07 +0530

    simple function program

--- function.sh
+++ function.sh
@@ -3,14 +3,10 @@
 # -eq equal, -ne not equal, -gt	greater than, -lt less than, -ge greater or equal, -le	less or equal
 # -u it only displays user id details not gid or groups id
 
-
-
-
 userid=$(id -u)
 echo "userid is $userid"
 
 VALIDATE() {
-
 if [ $1 -ne 0 ]
 then
 echo "$2 is FAILED"
@@ -26,26 +22,20 @@ echo "please switch to root user and rerun the program"
 exit 1
 fi
 
+dnf list installed git
+if [ $? -ne 0 ]
+then
+echo "git is not installed, installing.... it now"
+dnf install git -y
+VALIDATE $? git
+fi
+
 dnf list installed mysql-server
-
-VALIDATE $?
-
-# if [ $? -ne 0 ]
-# then
-
+if [ $? -ne 0 ]
+then
 echo "mysql-server is not installed, installing.... it now"
-
 dnf install mysql-server -y
+VALIDATE $? "mysql-server"
 
-VALIDATE $?
-# if [ $? -ne 0 ]
-# then
-# echo "mysql-server installation was failed.. check logs"
-# exit 1
-# else
-# echo "mysql-server was successfully installed"
-# fi
 
-# else
-# echo "my-sql was already installed"
-# fi
+
