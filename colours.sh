666a9f859fe7e3f9d8de84b5a003d11801c7c573
JagathReddy <jagathsaipj81@gmail.com>
2026-02-21 20:23:03 +0530

    adding colours on program

--- colours.sh
+++ colours.sh
@@ -14,10 +14,10 @@ echo "userid is $userid"
 VALIDATE() {
 if [ $1 -ne 0 ]
 then
-echo -e "$2 is  $R FAILED$N"
+echo -e "$2 is $R FAILED$N"
 exit 1
 else
-echo -e "$2 is  $G SUCCESS$N"
+echo -e "$2 is $G SUCCESS$N"
 fi
 }
 
@@ -39,6 +39,6 @@ dnf list installed mysql-server
 if [ $? -ne 0 ]
 then
 echo "mysql-server is not installed, installing.... it now"
-dnf install mysql-serverr -y
+dnf install mysql-server -y
 VALIDATE $? "mysql-server installation"
 fi
