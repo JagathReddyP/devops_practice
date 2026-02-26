4971ee1de9f9b9c17f7634f0e90d172a034ea001
JagathReddy <jagathsaipj81@gmail.com>
2026-02-27 04:57:55 +0530

    redirectories

--- redirectories.sh
+++ redirectories.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
+# 1> --> redirects only success output
+# 2> --> redirects only failure output
+# &> --> redirects everything irrespective of success or failure
+# >> --> it wont override previous data it appends the existing data ex: &>> , ls -l 2>> output.txt
+# tee --> wite logs to multiple destinations
+
 
 LOGS_FOLDER="/var/log/shell-script"
+#SCRIPT_NAME=$(echo "$0"|cut -d "." -f1)  
+SCRIPT_NAME=$(basename "$0" .sh)
+TIMESTAMP=$(date +%Y-%m-%d_%H-%M-%S)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME-$TIMESTAMP.log"
+mkdir -p "$LOGS_FOLDER"
 
 R="\e[31m"
 G="\e[32m"
@@ -9,33 +20,49 @@ N="\e[0m"
 USERID=$(id -u)
 
 CHECK_ROOT() {
-    if [ $USERID -ne 0 ]
+    if [ "$USERID" -ne 0 ]
     then
-     echo -e " $R you are not a root user, switch to root user $N "
+     echo -e " $R you are not a root user, switch to root user $N "|tee -a "$LOG_FILE"
      exit 1
     fi
 }
 
 VALIDATE() {
-    if [ $1 -ne 0 ]
+    if [ "$1" -ne 0 ]
     then
-     echo -e " $2 installation was $R failed $N "
+     echo -e " $2 installation was $R failed $N "|tee -a "$LOG_FILE"
     else
-     echo -e " $2 installation was $G Success $N"
+     echo -e " $2 installation was $G Success $N"|tee -a "$LOG_FILE"
     fi
 }
 CHECK_ROOT
 
-  for package in $@
+USAGE(){
+echo -e "$R USAGE : $N sh redirectories.sh package1 package2..."
+exit 1
+}
+
+echo "script started executing at : $(date)"|tee -a "$LOG_FILE"
+
+
+
+#  $# --> no of arguments passed to the script
+if [ $# -eq 0 ]
+then
+ USAGE
+ fi
+
+# $@ refers to all arguments paased to the script
+  for package in "$@"
    do
-    dnf list installed $package
+    dnf list installed "$package" &>> "$LOG_FILE"
   if [ $? -ne 0 ]
    then
-     echo -e " $package $G was not installed, going to install now $N"
-     dnf install $package -y
-     VALIDATE $? $package
+     echo -e " $package $G was not installed, going to install now $N"|tee -a "$LOG_FILE"
+     dnf install "$package" -y &>>"$LOG_FILE"
+     VALIDATE $? "$package"
    else
-     echo -e " $package $G was already installed nothing to do $N"
+     echo -e " $package $G was already installed nothing to do $N"|tee -a "$LOG_FILE"
    fi
    done
 
