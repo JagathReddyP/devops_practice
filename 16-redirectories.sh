fc512dba525872813adad269fa684205958cd0de
JagathReddy <jagathsaipj81@gmail.com>
2026-02-28 17:31:42 +0530

    renaming files

--- 16-redirectories.sh
+++ 16-redirectories.sh
@@ -32,6 +32,7 @@ VALIDATE() {
     if [ "$1" -ne 0 ]
     then
      echo -e " $2 installation was $R failed $N "|tee -a "$LOG_FILE"
+     exit 1
     else
      echo -e " $2 installation was $G Success $N"|tee -a "$LOG_FILE"
     fi
