aee8a23e076b5129f648fdb0a3bb9d60938ff88c
JagathReddy <jagathsaipj81@gmail.com>
2026-02-28 18:44:57 +0530

    renaming files

--- 17-set.sh
+++ 17-set.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-set -ex
+set -e # setting the automatic exit, if we get error, set -ex for debug
+
+failure(){
+echo "Failed at : $1:$2"
+}
+trap 'failure "${LINENO}" "$BASH_COMMAND"' ERR #ERR is the error signal
 echo "hello world success"
 echoo "hellow world failure"
 echo "hello world after failure"
